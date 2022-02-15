b9bd27609b02989cfbd9ea0fb9f067e91b63fb17
Oleh Volkolupov <olehvolkolupov@gmail.com>
2022-02-15 18:40:31 +0100

    update

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -2,7 +2,7 @@ import "lodash";
 import { alert } from "@pnotify/core";
 import "@pnotify/core/dist/BrightTheme.css";
 import "@pnotify/core/dist/PNotify.css";
-import "./styles.css"
+import "./styles.css";
 
 class CountryInfo {
   constructor(uri, refs) {
@@ -11,97 +11,97 @@ class CountryInfo {
   }
 
   createTable(data) {
-    let table = document.createElement('table');
+    let table = document.createElement("table");
     table.classList.add("table");
 
-    let tableBody = document.createElement('tbody');
+    let tableBody = document.createElement("tbody");
     tableBody.classList.add("table__body");
-  
-      let row1 = document.createElement('tr');
-      row1.innerHTML += `<td>Country:</td>`;
-      row1.innerHTML += `<td>${data.name.official}</td>`;
-      tableBody.append(row1);
 
-      let row2 = document.createElement('tr');
-      row2.innerHTML += `<td>Capital:</td>`;
-      row2.innerHTML += `<td>${data.capital[0]}</td>`;
-      tableBody.append(row2);
+    let row1 = document.createElement("tr");
+    row1.innerHTML += `<td>Country:</td>`;
+    row1.innerHTML += `<td><b>${data.name.common}</b></td>`;
+    tableBody.append(row1);
 
-      let row3 = document.createElement('tr');
-      row3.innerHTML += `<td>Population:</td>`;
-      row3.innerHTML += `<td>${data.population}</td>`;
-      tableBody.append(row3);
+    let row1a = document.createElement("tr");
+    row1a.innerHTML += `<td>Official name:</td>`;
+    row1a.innerHTML += `<td>${data.name.official}</td>`;
+    tableBody.append(row1a);
 
-      let row4 = document.createElement('tr');
-      row4.innerHTML += `<td>Languages:</td>`;
-      let langArray = Object.values(data.languages);
-      if(langArray.length === 1){
-        row4.innerHTML += `<td>${langArray[0]}</td>`;
-      } else {
-        let str = langArray.map(language =>language);
-        row4.innerHTML += `<td>${str}</td>`;
-      }
+    let row2 = document.createElement("tr");
+    row2.innerHTML += `<td>Capital:</td>`;
+    row2.innerHTML += `<td>${data.capital[0]}</td>`;
+    tableBody.append(row2);
 
-      tableBody.append(row4);
+    let row3 = document.createElement("tr");
+    row3.innerHTML += `<td>Population:</td>`;
+    row3.innerHTML += `<td>${data.population}</td>`;
+    tableBody.append(row3);
 
-      let row5 = document.createElement('tr');
-      row5.innerHTML += `<td>Flag:</td>`;
-      row5.innerHTML += `<td><img src=${data.flags.png} alt="Flag image"</td>`;
-      tableBody.append(row5);
+    let row4 = document.createElement("tr");
+    row4.innerHTML += `<td>Languages:</td>`;
+    let langArray = Object.values(data.languages);
+    if (langArray.length === 1) {
+      row4.innerHTML += `<td>${langArray[0]}</td>`;
+    } else {
+      let str = langArray.map((language) => language);
+      row4.innerHTML += `<td>${str}</td>`;
+    }
 
-      let row6 = document.createElement('tr');
-      row6.innerHTML += `<td>Coat of Arms:</td>`;
-      row6.innerHTML += `<td><img src=${data.coatOfArms.png} alt="Flag image"</td>`;
-      tableBody.append(row6);
+    tableBody.append(row4);
 
-      table.append(tableBody);
+    let row5 = document.createElement("tr");
+    row5.innerHTML += `<td>Flag:</td>`;
+    row5.innerHTML += `<td><img src=${data.flags.png} alt="Flag image"</td>`;
+    tableBody.append(row5);
 
-      this.refs.dataRef.append(table);
-     }
+    let row6 = document.createElement("tr");
+    row6.innerHTML += `<td>Coat of Arms:</td>`;
+    row6.innerHTML += `<td><img src=${data.coatOfArms.png} alt="Flag image"</td>`;
+    tableBody.append(row6);
+
+    table.append(tableBody);
+
+    return table;
+  }
+
+  createList(dataArr) {
+    let ul = document.createElement("ul");
+    let dataItems = dataArr.map(({ name }) => {
+      let li = document.createElement("li");
+      // let a = document.createElement("a");
+      let h2 = document.createElement("h2");
+
+      // a.href = "";
+      h2.textContent = name.common;
+      // a.append(h2);
+      // li.append(a);
+      li.append(h2);
+
+      return li;
+    });
+
+    ul.append(...dataItems);
+
+    return ul;
+  }
 
   // название, столица, население, языки и флаг.
-  renderInfo = (info) => {
-    // let data = info[0];
+  renderInfo = (infoArr) => {
     this.refs.dataRef.innerHTML = "";
+    let infoArrLength = infoArr.length;
 
-    this.createTable(info[0]);
-/*
-    // название страны
-    let contryName = document.createElement("h1");
-    contryName.textContent = data.name.official;
-    this.refs.dataRef.append(contryName);
-
-    // столица
-    let capital = document.createElement("h2");
-    capital.textContent = "Capital: " + data.capital[0]; 
-    this.refs.dataRef.append(capital);
-
-    // население
-    let population = document.createElement("h3");
-    population.textContent = "Population: " + data.population; 
-    this.refs.dataRef.append(population);
-
-    // языки languages
-    let languages = document.createElement("p");
-    let langArray = Object.values(data.languages);
-    let h3Ref = document.createElement("h3");
-    h3Ref.textContent = "Languages:"
-    languages.append(h3Ref);
-    langArray.forEach(elem => languages.append(elem));
-    this.refs.dataRef.append(languages);
-
-    // флаг
-    let flagImg = document.createElement("img");
-    flagImg.src = data.flags.png;
-    flagImg.alt = "Flag image";
-    this.refs.dataRef.append(flagImg);
-
-    // герб
-    let coatOfArmsImg = document.createElement("img");
-    coatOfArmsImg.src = data.coatOfArms.png;
-    coatOfArmsImg.alt = "Coat of arms image";
-    this.refs.dataRef.append(coatOfArmsImg);
-*/
+    if (infoArrLength > 10) {
+      alert({
+        text: "Укажите более точное название страны.",
+        type: "notice",
+      });
+    } else {
+      this.refs.dataRef.append(
+        infoArrLength > 2
+          ? this.createList(infoArr)
+          : this.createTable(infoArr[0])
+      );
+    }
   };
 
   fetchCountryInfo = (inputValue) => {
@@ -117,23 +117,7 @@ class CountryInfo {
         return res.json();
       })
       .then((data) => {
-        let dataLength = data.length;
-        if (dataLength > 10) {
-          alert({
-            text: "Укажите более точное название страны.",
-            type: "notice",
-          });
-          return null;
-        } else if (dataLength > 2) {
-          this.refs.dataRef.innerHTML = "";
-          data.forEach( elem => {
-            let name = document.createElement("h2")
-            name.textContent = elem.name.official;
-            this.refs.dataRef.append(name);
-          });
-        } else {
-          this.renderInfo(data);
-        }
+        this.renderInfo(data);
       })
       .catch((err) => {
         alert({
